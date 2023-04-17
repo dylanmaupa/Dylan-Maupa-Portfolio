6187d7f9b7eee8bbd2a80c8d910f041514dc3d3e
dylanmaupa <114060288+dylanmaupa@users.noreply.github.com>
2023-04-17 15:49:28 +0200

    #

--- js/main.js
+++ js/main.js
@@ -1,42 +1,36 @@
-const css = document.getElementById('css');
-const tags = document.getElementById('tags');
-const html = document.getElementById('html');
-const xd = document.getElementById('xd');
-const js = document.getElementById('js');
-const wp = document.getElementById('wp');
-const memoji = document.getElementById("memoji");
+// const css = document.getElementById("css");
+// const tags = document.getElementById("tags");
+// const html = document.getElementById("html");
+// const xd = document.getElementById("xd");
+// const js = document.getElementById("js");
+// const wp = document.getElementById("wp");
+// const memoji = document.getElementById("memoji");
 const greyRect = document.getElementById("grey-rect");
 const greyRect2 = document.getElementById("grey-rect2");
+const greyRect3 = document.getElementById("grey-rect3");
+const greyRect4 = document.getElementById("grey-rect4");
 // let heroText = document.getElementsByClassName("hero-text");
 
-window.addEventListener('scroll', () => {
-    let value = window.scrollY;
+window.addEventListener("scroll", () => {
+  let value = window.scrollY;
 
-    css.style.marginLeft = value * -0.5 + 'px';
-
-    tags.style.marginLeft = value * -1.5 + 'px';
-
-    memoji.style.marginTop = value * -1.5 + 'px';
-
-    html.style.marginTop = value * -0.5 + 'px';
-    html.style.marginRight = value * -0.5 + 'px';
-
-    xd.style.marginRight = value * -0.5 + 'px';
-
-    js.style.marginRight = value * -1 + 'px';
-
-    wp.style.marginTop = value * -1.5 + 'px';
-    wp.style.marginRight = value * -0.5 + 'px';
-
-    greyRect.style.rotate = value * 0.07 + 'deg';
-    greyRect2.style.rotate = value * 0.09 + 'deg';
+  greyRect.style.rotate = value * 0.07 + "deg";
+  greyRect2.style.rotate = value * 0.09 + "deg";
+  greyRect3.style.rotate = value * 0.11 + "deg";
+  greyRect4.style.rotate = value * 0.13 + "deg";
 });
 
-ScrollReveal().reveal(".skill-card, #about,.img,.grey-rect", { delay: 200, easing: "ease-out", interval: 200, scale: 1.2 });
+ScrollReveal().reveal(".skill-card, #about,.img,.grey-rect", {
+  delay: 200,
+  easing: "ease-out",
+  interval: 200,
+  scale: 1.2,
+});
 
 const hMenu = document.getElementById("hMenu");
+const nav = document.querySelector(".nav");
+// const menu = document.querySelector(".menu");
 
-// hMenu.onclick = function(){
-//     const nav = document.querySelector(".nav");
-    
-// }
+hMenu.onclick = function () {
+  nav.classList.toggle("active");
+};
